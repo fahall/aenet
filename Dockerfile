208b9b1ba4eff03996b231fb14af5f067291c520
Alex Hall <alex@alexhall.org>
2018-06-25 13:22:26 -0700

    added ffmpeg to docker and preprocessing code for .m4a -> .wav

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,15 @@
 FROM nvidia/cuda:8.0-cudnn6-devel-ubuntu16.04
 
+RUN apt-get update && apt-get install -y rsync htop git openssh-server
+RUN apt-get install -y build-essential libpq-dev libssl-dev openssl libffi-dev zlib1g-dev
+RUN apt-get install -y wget software-properties-common
+
+# ffmpeg
+RUN add-apt-repository -y ppa:jonathonf/ffmpeg-3
+RUN apt install -y ffmpeg
 
 # Python Basics
-RUN apt-get update && apt-get install -y rsync htop git openssh-server python-pip
+RUN apt-get install -y python-pip
 RUN pip install --upgrade pip
 RUN pip install tqdm moviepy requests pydub
 
@@ -10,9 +17,6 @@ RUN pip install tqdm moviepy requests pydub
 RUN pip install -r https://raw.githubusercontent.com/Lasagne/Lasagne/master/requirements.txt
 RUN pip install https://github.com/Lasagne/Lasagne/archive/master.zip
 
-# ffmpeg
-RUN add-apt-repository -y ppa:jonathonf/ffmpeg-3
-RUN apt install -y ffmpeg
 
 
 # Run AENet downloads
@@ -21,5 +25,5 @@ VOLUME ["/data"]
 ADD . /aenet
 ENV AENET_DATA_DIR /data/aenet
 RUN /aenet/bin/download.sh
-RUN python /aenet/setup.py install
+# RUN python /aenet/setup.py install
 
--- preprocess_audio.py
+++ preprocess_audio.py
@@ -1,13 +1,35 @@
-import subprocess
-from pydub import AudioSegment
-from glob import glob
 import os.path as osp
-DATA_HOME = '/app/data/'
+from glob import glob
+
+from pydub import AudioSegment
+from tqdm import tqdm
+
+DATA_HOME = '/data/'
 AUDIO_RAW = osp.join(DATA_HOME, 'audio_raw')
 AUDIO_OUT = osp.join(DATA_HOME, 'audio')
 
 
+def infile_to_outfile(infile, filetype='.wav'):
+    title = osp.splitext(osp.basename(infile))[0]
+    return osp.join(AUDIO_OUT, title + filetype)
+
+
+def convert(infile, new_file_extension='.wav'):
+    old_ext = osp.splitext(infile)[1][1:]  # remove leading '.'
+
+    outfile = infile_to_outfile(infile, new_file_extension)
+    if not osp.exists(outfile):
+        try:
+            sound = AudioSegment.from_file(infile, format=old_ext)
+            # remove leading '.'
+            return sound.export(outfile, format=new_file_extension[1:])
+        except:
+            with open('falures.txt', 'a') as f:
+                f.write(outfile + '\n')
+
 
 if __name__ == "__main__":
     audio_files = glob(osp.join(AUDIO_RAW, '*.m4a'))
-    print(audio_files)
+
+    for a in tqdm(audio_files):
+        convert(a)
